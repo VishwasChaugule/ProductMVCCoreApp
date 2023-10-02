5a6a45104283502b69344ed45ca6d53feba346bf
Vishwas Chaugule <chaugule1369vish@gmail.com>
2023-10-02 20:42:49 +0530

    Initial Commit

---
+++ ProductMVCCoreApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductMVCCoreApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
